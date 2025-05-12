7aa5c3ce8130e1422d46dc8858764d6096f41f45
bagus arif arikusworo <bagusarifproject@gmail.com>
2025-05-12 21:51:35 +0700

    pengujian linear 1

--- run_all_test.js
+++ run_all_test.js
@@ -4,9 +4,11 @@ const testFolders = ['infinix', 'note_50_pro'];
 testFolders.forEach(folder => {
   console.log(`\nRunning tests in folder: ${folder}`);
   try {
-    execSync(`npx cypress run --spec "cypress/e2e/${folder}/*.cy.js"`, { stdio: 'inherit' });
+    execSync(`npx cypress run --browser chrome --spec "cypress/e2e/${folder}/*.cy.js"`, {
+      stdio: 'inherit',
+    });
   } catch (error) {
     console.error(`Error occurred while running tests in ${folder}`);
-    process.exit(1); 
+    process.exit(1);
   }
 });
