b329d849e101fdba8f25117dd7a9dc40d2dd93c3
bagus arif arikusworo <bagusarifproject@gmail.com>
2025-05-13 22:36:13 +0700

    pengujian linear 1

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,16 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser = {}, launchOptions) => {
         if (browser.name === 'chrome') {
-          launchOptions.args.push('--no-sandbox')
-          launchOptions.args.push('--disable-gpu')
-          launchOptions.args.push('--disable-dev-shm-usage')
+          launchOptions.args.push('--no-sandbox');
+          launchOptions.args.push('--disable-dev-shm-usage');
+          launchOptions.args.push('--disable-gpu');
         }
         return launchOptions;
       });
-    }
-  }
-};
+    },
+  },
+});
